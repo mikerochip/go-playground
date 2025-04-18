4b130be20f6f68df506ac31c4280926bd614754c
Mike Schweitzer <mschweitzer@gmail.com>
2025-04-17 22:00:59 -0700

    Make math_test more flexible

--- math_test/math_test.go
+++ math_test/math_test.go
@@ -7,9 +7,10 @@ import (
 )
 
 func TestAdd(t *testing.T) {
-	got := math.Add(2, 3)
-	want := 5
+	var got, want int
 
+	got = math.Add(2, 3)
+	want = 5
 	if got != want {
 		t.Errorf("Add(2, 3) = %d; want %d", got, want)
 	}
